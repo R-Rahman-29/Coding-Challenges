e20ed48505dcee33dcf9804730d6d6c3828ddc6f
Rayan Rahman <redknight2926@gmail.com>
2026-02-08 01:23:36 +0000

    Wrote code for the grid
    Got the grid to appear in the window and also wrote the code for cells to appear yellow, at the moment it has test code to show cell (10,10) is working and appears yellow.

--- Conway's game of life/gameOfLife.py
+++ Conway's game of life/gameOfLife.py
@@ -16,36 +16,57 @@ FPS = 60
 #Function for setting up the screen
 def screenSetUp():
     #Setting up the screen
+    global screen, clock #Declaring screen and clock as global variables
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Conway's Game of Life")
     clock = pygame.time.Clock()
+    pygame.display.set_caption("Rayan's Game of Life")
 
-    return screen, clock
+
+#Function for drawing the grid
+def drawGrid(positions):
+    drawRec(positions)
+
+    #Draws horizontal lines
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+    
+    #Draws vertical lines
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
+
+#Function for drawing each cell
+def drawRec(positions):
+    for position in positions:
+        col, row = position
+        topLeft = (col * TILE_SIZE, row * TILE_SIZE)
+        pygame.draw.rect(screen, YELLOW, (*topLeft, TILE_SIZE, TILE_SIZE))
 
 #Defining main function
 def main():
+    running = True
+    positions = set()
+    positions.add((10, 10))
+
     #Initialising pygame
     pygame.init()
     
     #Calling screen setup function
-    screen, clock = screenSetUp()
+    screenSetUp()
 
     #Game loop
-    running = True
     while running:
-        clock.tick(FPS)
+        clock.tick(FPS) #Controls the frame rate
 
+        #Closes the window if the user clicks the close button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill(BLACK)
-        pygame.display.update()
+        screen.fill(GREY) #Defines background colour of the window
+        drawGrid(positions) #Draws the grid
+        pygame.display.update() #Updates the display
 
-        pygame.display.flip()
-
-    pygame.quit()
+    pygame.quit() #Closes window
 
 if __name__ == "__main__":
     main()
-
